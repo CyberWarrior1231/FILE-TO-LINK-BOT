52a66d1dee7f7689ace4a535a1cd8eef644c1eee
CyberWarrior1231 <thestaralwayad@gmail.com>
2026-02-25 15:30:19 +0530

    Update info.py

--- info.py
+++ info.py
@@ -24,7 +24,7 @@ BIN_CHANNEL = int(environ.get("BIN_CHANNEL", '-1003689436454'))
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", '-'))
 PREMIUM_LOGS = int(environ.get("PREMIUM_LOGS", '-1003689436454'))
 VERIFIED_LOG = int(environ.get('VERIFIED_LOG', '-1003689436454'))
-SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "https://t.me/Theprimevault01"))
+SUPPORT_GROUP = int(environ.get("SUPPORT_GROUP", "-1003689436454"))
 
 # ✅ __Feature Toggles__
 VERIFY = False  # Enable user verification
